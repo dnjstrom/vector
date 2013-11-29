d1a4e7fec717f4449deaa9f604acda9773e27fcb
Daniel Ström <D@nielstrom.se>
2013-11-29 11:19:26 +0100

    Create a basic implementation of a 2D vector library.

---
+++ lib/vector.rb
@@ -0,0 +1,94 @@
+class Vector
+	attr_accessor :angle, :length, :mode
+
+	def initialize(mode = :deg)
+		@mode = mode
+		@angle = 0
+		@length = 1
+	end
+
+	def copy
+		Vector.new.set(x, y)
+	end
+
+	def turn(angle, mode=@mode)
+		if mode == :rad
+			angle = Vector.r2d angle
+		end
+
+		@angle +=angle
+		self
+	end
+
+	def normalize
+		@length = 1
+	end
+
+	def +(v)
+		v.copy.add(x, y)
+	end
+
+	def -(v)
+		v.copy.add(-x, -y)
+	end
+
+	def *(v)
+		if v.class <= Vector
+			# dot product, maybe should change
+			@length * v.length * Math.cos(Vector.r2d(@angle - v.angle))
+		else # scaling with a number
+			scale(v)
+		end
+	end
+
+	def add(x, y)
+		set(self.x + x, self.y + y)
+	end
+
+	def add!(x, y)
+		copy.add(x, y)
+	end
+
+	def scale(scalar)
+		set(x*scalar, y*scalar)
+	end
+
+	def scale!(scalar)
+		copy.scale(scalar)
+	end
+
+	def set(x, y)
+		@length = Math.sqrt(x*x + y*y)
+		@angle = Vector.r2d Math.atan2(y, x)
+		self
+	end
+
+	def ratio
+		(x / y).abs
+	end
+
+	def x
+		@length * Math.cos(Vector.d2r(@angle))
+	end
+
+	def x=(newX)
+		set newX, y
+	end
+
+	def y
+		@length * Math.sin(Vector.d2r(@angle))
+	end
+
+	def y=(newY)
+		set x, newY
+	end
+
+
+	def self.r2d(rad)
+		rad * (180/Math::PI)
+	end
+
+	def self.d2r(deg)
+		deg * (Math::PI/180)
+	end
+end
--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -1,7 +1,70 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Vector" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Vector do
+	before :each do
+  	@v = Vector.new
+	end
+
+  it "initializes to a normalized vector" do
+  	@v.length.should eq 1
+  end
+
+  it 'initializes to a vector with angle 0' do
+  	@v.angle.should eq 0
+  end
+
+  it 'returns the correct x component of the vector' do
+  	@v.x.should eq 1
+  end
+
+  it 'returns the correct y component of the vector' do
+  	@v.y.should eq 0
+  end
+
+  it 'sets the vector to the correct x and y' do
+  	@v.set 3, 4
+
+  	@v.length.should eq 5
+  	@v.angle.should be_within(0.01).of(53.13)
+  end
+
+  it 'sets the vectors individual components correctly' do
+  	@v.x = 3
+  	@v.x.should be_within(0.0000001).of(3)
+
+  	@v.y = 4
+  	@v.y.should be_within(0.0000001).of(4)
+  end
+
+  it 'can turn the vector using degrees' do
+  	@v.turn 30
+  	@v.angle.should eq 30
+  end
+
+  it 'can turn the vector using radians' do
+  	@v.turn Math::PI, :rad
+  	@v.angle.should eq 180
+  end
+
+  it 'two vectors can be added to each other' do
+  	vv = Vector.new
+  	vvv = @v + vv
+  	vvv.length.should eq 2
+  end
+
+  it 'can be modified' do
+  	@v.add(3, 4)
+  	@v.x.should be_within(0.0000001).of(4)
+  	@v.y.should be_within(0.0000001).of(4)
+  end
+
+  it 'can be normalized' do
+  	@v.normalize
+  	@v.length.should eq 1
+  end
+
+  it 'can be multiplied with a scalar' do
+  	@v * 10
+  	@v.length.should eq 10
   end
 end
